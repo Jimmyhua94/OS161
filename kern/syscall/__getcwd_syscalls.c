401595c86b5ef9b50a8db3df3ca62902ce4213af
Jimmyhua <jimmyhua@buffalo.edu>
2016-03-11 21:22:10 -0500

    submit

--- kern/syscall/__getcwd_syscalls.c
+++ kern/syscall/__getcwd_syscalls.c
@@ -13,19 +13,16 @@ int sys___getcwd(userptr_t buf,size_t buflen,int32_t *retval){
         return EFAULT;
     }
     
-    struct iovec *iov = kmalloc(sizeof(*iov));
-    struct uio *u = kmalloc(sizeof(*u));
-    uio_kinit(iov,u,(void *)buf,buflen,0,UIO_READ);
+    struct iovec iov;
+    struct uio u;
+    uio_kinit(&iov,&u,(void *)buf,buflen,0,UIO_READ);
     
     result = vfs_getcwd(u);
     if(result){
         return result;
     }
     
-    *retval = buflen - u->uio_resid;
-    
-    kfree(iov);
-    kfree(u);
+    *retval = buflen - u.uio_resid;
     
     return 0;
 }
